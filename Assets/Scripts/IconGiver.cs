28191dd2976fe480a124caede344863451b13580
stoicalsilence <alexwurdegescamt@gmail.com>
2023-04-08 19:18:54 +0200

    IconGiver + icons

---
+++ Assets/Scripts/IconGiver.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconGiver : MonoBehaviour
+{
+    //{ STONE, THUNDER, MACHINE, ROCK, FIRE, WATER, DRAGON, WARRIOR, FAIRY, INSECT, ZOMBIE, BEAST, PLANT, WINGEDBEAST, SPELLCASTER }
+    public Material warriorIcon;
+    public Material spellcasterIcon;
+    public Material stoneIcon;
+    public Material plantIcon;
+    public Material zombieIcon;
+    public Material wingedbeastIcon;
+    public Material dragonIcon;
+    public Material fireIcon;
+    public Material machineIcon;
+    public Material thunderIcon;
+    public Material waterIcon;
+    public Material fairyIcon;
+    public Material beastIcon;
+    public Material insectIcon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
